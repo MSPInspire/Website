418e01440290b0e891aaaaca0ed012194486a979
SalmanMKC <13schishti@gmail.com>
2020-04-03 12:33:31 +0100

    set up Team member class and Teams

---
+++ Data/Team.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Team
+    {
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string secondName;
+
+        public string SecondName
+        {
+            get { return secondName; }
+            set { secondName = value; }
+        }
+
+        private string subtitle;
+
+        public string Subtitle
+        {
+            get { return subtitle; }
+            set { subtitle = value; }
+        }
+
+
+        private string picture;
+
+        public string Picture
+        {
+            get { return picture; }
+            set { picture = value; }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Data/Teams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Teams
+    {
+        static List<Team> teams = new List<Team>()
+        {
+            new Team
+            {
+                FirstName = "Salman",
+                SecondName = "Chishti",
+                Picture = "https://pbs.twimg.com/profile_images/1240958813114699777/fuqzzP9o_400x400.png",
+                Subtitle = "Incoming Microsoft Intern, MSP"
+            },
+            new Team
+            {
+                FirstName = "Dawood",
+                SecondName = "Iddris",
+                Picture = "https://pbs.twimg.com/profile_images/1243281133128867841/qo8YAswd_400x400.jpg",
+                Subtitle = "Co-founder of techloud, MSP"
+            },
+            new Team
+            {
+                FirstName = "Sabiha",
+                SecondName = "Shaik",
+                Picture = "https://pbs.twimg.com/profile_images/1237997999848112128/afh2nqnU_400x400.jpg",
+                Subtitle = "Co-founder of zuvmeco, MSP"
+            }
+        };
+    }
+}

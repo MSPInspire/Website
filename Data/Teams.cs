b4051a99ed69eb3b07431f3c94651f9f5d0bd339
Sunder Eshwar <54266689+sundereshwar@users.noreply.github.com>
2020-04-05 12:25:39 +0530

    Update in Sabiha's Name

--- Data/Teams.cs
+++ Data/Teams.cs
@@ -13,7 +13,7 @@ namespace BlazorApp1.Data
             
             Team salman = new Team("Salman", "Chishti", "incoming Microsoft intern", "https://pbs.twimg.com/profile_images/1240958813114699777/fuqzzP9o_400x400.png");
             Team dawood = new Team("Dawood", "Iddris", "Co-founder of Techloud, MSP", "https://pbs.twimg.com/profile_images/1243281133128867841/qo8YAswd_400x400.jpg");
-            Team sabiha = new Team("Salman", "Chishti", "Co-founder of zuvmeco, MSP ", "https://pbs.twimg.com/profile_images/1237997999848112128/afh2nqnU_400x400.jpg");
+            Team sabiha = new Team("Sabiha", "Shaik", "Co-founder of zuvmeco, MSP ", "https://pbs.twimg.com/profile_images/1237997999848112128/afh2nqnU_400x400.jpg");
             Team sunder = new Team("Sundereshwar", "Kodi", "MSP", "https://pbs.twimg.com/profile_images/996210938746372096/FcxJdvpu_400x400.jpg");
             teams.Add(salman);
             teams.Add(dawood);

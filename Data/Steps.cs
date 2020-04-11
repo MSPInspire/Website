7dc3767336c34246dda10c5db83a118e0b68ebfd
SalmanMKC <13schishti@gmail.com>
2020-04-11 08:20:15 +0100

    Update Steps.cs

--- Data/Steps.cs
+++ Data/Steps.cs
@@ -7,15 +7,15 @@ namespace BlazorApp1.Data
 {
     public class Steps
     {
-        List<Step> steps = new List<Step>()
-        {
-            new Step
-            (
-                Instruction = "Visit https://studentpartners.microsoft.com/",
-                Img = "/Assets/Certificate/SignIn.jpg",
-                Caption = "Log in in the top right"
-            ),
+        //List<Step> steps = new List<Step>()
+        //{
+        //    new Step
+        //    (
+        //        Instruction = "Visit https://studentpartners.microsoft.com/",
+        //        Img = "/Assets/Certificate/SignIn.jpg",
+        //        Caption = "Log in in the top right"
+        //    ),
 
-        }
+        //};
     }
 }
